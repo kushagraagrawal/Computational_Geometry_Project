b1fd5a73fba32de957be00ce1d56548dcf6534a1
Rishabh Pokharna <rishabhpokharna16@gmail.com>
2017-02-14 17:55:46 +0530

    Merged 3 main files,modified makefile

--- main.cpp
+++ main.cpp
@@ -25,19 +25,38 @@ int main(int argc,char *argv[]){
 
 	std::cout<<std::setprecision(15);
 	
+	char *input_file = argv[1];			// file to read input points
+	char *output_file = argv[2];		// file to write Convex Hull
+	int choice = atoi(argv[3]);			// choice to determine which algorithm to use
+	std::vector<cg::Point> point_set; 	// contains the input points
+	std::vector<cg::Point> ch;			// contains the Convex hull of input points
+	
 	// Read from file
-	std::vector<cg::Point> p;
-	cg::readPointSet(argv[1],p);
+
+	cg::readPointSet(input_file,point_set);
+	
 	std::cout << "Read file successfully\n";
 	
 	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-	std::vector<cg::Point> ch = cg::convexHullGrahamScan(p);
-	std::cout<<"Completed convex hull using Graham Scan.\n";
+	
+	switch(choice){
+		case 1:	ch = cg::convexHullGrahamScan(point_set);
+				std::cout<<"Completed convex hull using Graham's Scan.\n";	
+				break;
+		case 2: ch = cg::convexHullJarvisMarch(point_set);
+				std::cout<<"Completed convex hull using Jarvis March.\n";	
+				break;
+		case 3: ch = cg::convexHullAndrews(point_set);
+				std::cout<<"Completed convex hull using Andrew's Algorithm.\n";	
+				break;
+		default: std::cerr << "Third agruement should be 1 or 2 or 3\n";
+	}
+
 	std::chrono::steady_clock::time_point end= std::chrono::steady_clock::now();
 	std::cout << "Time taken to compute Convex Hull is = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count()<<" ms" <<std::endl;
 	
 	// write to file
-	cg::writePointSet(argv[2],ch);
+	cg::writePointSet(output_file,ch);
 	std::cout << "File written successfully.\n";
 	return 0;
 }
