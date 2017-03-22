2b525b9a7c19b2d83fc9ed23e6dc5c00d5a43dcd
Anand Ravi <anandravi711@gmail.com>
2017-03-22 13:37:31 +0530

    status implemented via STL

---
+++ Status.cpp
@@ -0,0 +1,29 @@
+#include "Status.h"
+
+namespace cg {
+	void Status::insert(const std::pair<cg::Point, cg::Point>& edgeNew, int helper) {
+		M.insert(std::make_pair(edgeNew, helper));
+	}
+	
+	void Status::remove(const std::pair<cg::Point, cg::Point>& edgeRem) {
+		auto x = M.find(edgeRem);
+		if (x != M.end()) M.erase(x);
+	}
+	
+	int Status::getHelper(const std::pair<cg::Point, cg::Point>& edgeQuery) {
+		auto x = M.find(edgeQuery);
+		if (x != M.end()) return x->second;
+		return -1;
+	}
+	
+	void Status::setHelper(const std::pair<cg::Point, cg::Point>& edgeQuery, int helper) {
+		auto x = M.find(edgeQuery);
+		if (x != M.end()) x->second = helper;
+	}
+	
+	std::pair<cg::Point, cg::Point> Status::findEdgeToLeft(const cg::Point& v) {
+		auto x = M.upper_bound(std::make_pair(v, cg::Point(0,0)));
+		if (x != M.end()) return x->first;
+	}
+
+}
